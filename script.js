2e203ef337425b72f4b04fb46a252b51d8625d6f
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-19 10:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+ function indexOfIgnoreCase(s1, s2) {
+  const strLower = s1.toLowerCase();
+  const subStrLower = s2.toLowerCase();
+  const indexLimit = strLower.length - subStrLower.length;
+
+  for (let i = 0; i <= strLower.length; i++) {
+    let found = true;
+
+    for (let j = 0; j < subStrLower.length; j++) {
+      if (strLower[i + j] !== subStrLower[j]) {
+        found = false;
+        break;
+      }
+    }
+
+    if (!found) {
+      return indexLimit;
+    }
+  }
+
+  return -1;
 }
 
-// Please do not change the code below
+// Example usage
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
